67ea1a25653c163e113f56146c9a026b512be0a8
aakashks <114357454+aakashks@users.noreply.github.com>
2023-11-02 19:01:09 +0530

    changed endchar to space and adds hyphen

--- extract_links.sh
+++ extract_links.sh
@@ -9,6 +9,6 @@ input_file="$1"
 output_file="${input_file%.*}_links.txt"
 
 # Use grep with a regular expression to find all links starting with "https" and ending with an endline
-grep -oP 'https://[^\n]*' "$input_file" > "$output_file"
+grep -oP 'https://[^\ ]*' "$input_file" | sed 's/^/- /' > "$output_file"
 
 echo "Links extracted and saved to $output_file"
