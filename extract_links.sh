d1135d2b5399a587628a5b233d18f399d651aa28
aakashks <114357454+aakashks@users.noreply.github.com>
2023-11-03 10:22:24 +0530

    can't extract links w/o title

--- extract_links.sh
+++ extract_links.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# USING V1 IS SUGGESTED SINCE THIS DOESN'T ACCOUNT SOME POSSIBILITIES LIKE CONSECUTIVE LINKS
+
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 input_file"
   exit 1
 fi
 
 input_file="$1"
-output_file="${input_file%.*}_links.md"
+output_file="links_${input_file%.*}.md"
 
 # Empty the output file
 > $output_file
@@ -17,13 +19,15 @@ regex="https[^\ ]+"
 while read -r line; do
   if [[ $line =~ $regex ]]; then
     link="${BASH_REMATCH[0]}"
+    # echo "- $link" >> "$output_file"
+
     # Read the next line to get the title
     read -r discard
     read -r title
     # Write the title and link in the desired format to the output file
     
     # if the title is blank
-    if [[ $title =~ "^$" ]]; then
+    if [[ "$title" =~ ^$'\n'$ ]]; then
       echo "- $link" >> "$output_file"
     else
       echo "- [$title]($link)" >> "$output_file"
