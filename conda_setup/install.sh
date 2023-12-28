8f8c22c52f3237e4af01c7c9b0e4898173df9c84
aakashks <114357454+aakashks@users.noreply.github.com>
2023-12-28 21:55:55 +0530

    using makefile

--- conda_setup/install.sh
+++ conda_setup/install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# well conda is not detected on the system
+
 conda env create -f env_template.yml
 conda activate py311
 # installing pytorch
 conda install -y pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 
-## after installing pytorch
-conda install -y lightning transformers -c conda-forge
+## after installing pytorch (packages with pytorch dependency)
+conda install -y lightning transformers timm -c conda-forge
